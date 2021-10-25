087fd714b45d71e6344f15052014207996bd5e05
gslab <gslab@GS-4945>
2021-10-25 17:13:37 +0530

    new change in branchX

--- src/main/java/com/javatpoint/mypackage/Employee.java
+++ src/main/java/com/javatpoint/mypackage/Employee.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 //POJO Class
+//change in BranchX
 @Entity
 @Table(name = "Emp")
 public class Employee {
