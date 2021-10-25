671bec3993faa201adeb4823bdc5ae641dff0b9a
RNM <ravindra.mahajan@gslab.com>
2021-10-25 19:14:12 +0530

    # WARNING: head commit changed in the meantime
    merge

--- src/main/java/com/javatpoint/mypackage/Employee.java
+++ src/main/java/com/javatpoint/mypackage/Employee.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 //POJO Class
+//branch A
 @Entity
 @Table(name = "Emp")
 public class Employee {
