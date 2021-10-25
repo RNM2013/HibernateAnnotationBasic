57aa8621a3c5e88705d21d9fc1bb4be7ae1c9ded
gslab <gslab@GS-4945>
2021-10-25 13:59:19 +0530

    First Commit

---
+++ src/main/java/com/javatpoint/mypackage/Employee.java
@@ -0,0 +1,48 @@
+package com.javatpoint.mypackage;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Emp")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column
+	private String firstName;
+	@Column
+	private String lastName;
+	@Column String email;
+	public String setEmail() {
+		return this.email=this.firstName+this.lastName+"@gslab.com";
+	}
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
