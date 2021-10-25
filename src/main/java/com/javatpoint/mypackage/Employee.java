1e9e0af90278f1d4d92902c33bdfb0064b1c4d8c
RNM <ravindra.mahajan@gslab.com>
2021-10-25 19:21:21 +0530

    comit

--- src/main/java/com/javatpoint/mypackage/Employee.java
+++ src/main/java/com/javatpoint/mypackage/Employee.java
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 //POJO Class
-//branch A pull change code added
+//branch A pull change code
 @Entity
 @Table(name = "Emp")
 public class Employee {
